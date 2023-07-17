927b05227859c9b79087a0557f9e7607fc47cea1
Andre Riyanto <andreriyantoo19@gmail.com>
2023-07-17 14:41:45 +0700

    new

--- arif.js
+++ arif.js
@@ -1,11 +1,33 @@
-let result;
+alert('Welcome to my app')
 
-function penjumlahan(a, b) {
-  if (operator == "penjumlahan") {
-    result = a + b;
-    return result;
-  }
+const value =prompt('Who is your name ?')
+alert(`Hello ${value}`)
+
+
+const verifikasi = confirm('Are you human ?')
+if(verifikasi == true){
+       alert('Verified ✔')
+}else {
+      alert('Not Verified ')
+        }
+
+const pilihan = prompt('1. Penjumlahan\n2. Pengurangan')
+
+        
+
+
+let result;
+switch(pilihan) {
+  case '1':
+    const a = prompt('firstNUmber')
+    const b = prompt('secondNumber')  
+    function penjumlahan(a, b) {
+      return a + b
+    }
+    alert(penjumlahan(a, b))
+    
 }
+
 function pengurangan(a, b) {
   if (operator == "penjumlahan") {
     result = a - b;
